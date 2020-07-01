64610f61d2079da9285fb1a5eb4acbcbf314b2c9
nkorovikov <nkorovikov@users.noreply.github.com>
2020-07-01 20:00:04 +0000

    deploy: 8337091235b814a71d1a83072f01327f64d8f138

---
+++ precache-manifest.1b4a9735c7fb7950ced445a040524437.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9efba9bfe0af40453e0c",
+    "url": "/css/app.b92f8377.css"
+  },
+  {
+    "revision": "6b4c205e531ec5e39598705a75248103",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9efba9bfe0af40453e0c",
+    "url": "/js/app.09c47e96.js"
+  },
+  {
+    "revision": "43da67a27f8b688b350e",
+    "url": "/js/chunk-3898dba7.346fc526.js"
+  },
+  {
+    "revision": "3ab2db9005c3f5a7ec35",
+    "url": "/js/chunk-vendors.99493b18.js"
+  },
+  {
+    "revision": "e3b5a23e2b3c77ac9e7f97eff12f9385",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
