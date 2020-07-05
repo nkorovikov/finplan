9083153288d22f6253f42c747be252c59786c3ac
nkorovikov <nkorovikov@users.noreply.github.com>
2020-07-05 09:53:41 +0000

    deploy: 030075b8229b18f8b183fe191b2f1fb6d6cfe357

---
+++ precache-manifest.1f9826de6c41682cc378a16ffb250a91.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f35bf30d52f89645732d",
+    "url": "/finplan/css/app.c777b7fc.css"
+  },
+  {
+    "revision": "4d05ee84a52841a2c290",
+    "url": "/finplan/css/chunk-vendors.3d1da9f7.css"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/finplan/fonts/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/finplan/fonts/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/finplan/fonts/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/finplan/fonts/MaterialIcons-Regular.e79bfd88.eot"
+  },
+  {
+    "revision": "be585b7964de5e00d25e191d254b00f0",
+    "url": "/finplan/img/splashes/apple_splash_750.png"
+  },
+  {
+    "revision": "dc968fd767ebbf09ddd2b3e6ae4b4b4d",
+    "url": "/finplan/index.html"
+  },
+  {
+    "revision": "f35bf30d52f89645732d",
+    "url": "/finplan/js/app.019ff721.js"
+  },
+  {
+    "revision": "95aeafe161fb4d2a6d0a",
+    "url": "/finplan/js/chunk-202e85e4.e6a64df5.js"
+  },
+  {
+    "revision": "be1348c06f905dc5e6aa",
+    "url": "/finplan/js/chunk-d7f8200e.41e57d26.js"
+  },
+  {
+    "revision": "4d05ee84a52841a2c290",
+    "url": "/finplan/js/chunk-vendors.ac333963.js"
+  },
+  {
+    "revision": "e3b5a23e2b3c77ac9e7f97eff12f9385",
+    "url": "/finplan/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/finplan/robots.txt"
+  },
+  {
+    "revision": "28bd4d8569c154c40efbaef521d37699",
+    "url": "/finplan/site.webmanifest"
+  }
+]);
