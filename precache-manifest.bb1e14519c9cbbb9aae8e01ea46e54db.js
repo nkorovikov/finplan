efe013978de000bdf08c93c8a0d9024518ded740
nkorovikov <nkorovikov@users.noreply.github.com>
2020-07-23 20:57:34 +0000

    deploy: 6cb52034c2800858385da90162605497c7db6948

---
+++ precache-manifest.bb1e14519c9cbbb9aae8e01ea46e54db.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c9cf2ec7f241780cce9",
+    "url": "/finplan/css/app.0e433876.css"
+  },
+  {
+    "revision": "35e0cf2fe043a164cc54",
+    "url": "/finplan/css/chunk-051ef01c.d58645f3.css"
+  },
+  {
+    "revision": "c2486de4b7816df65330",
+    "url": "/finplan/css/chunk-0f8afdc6.1f74ea8e.css"
+  },
+  {
+    "revision": "a6e24cd78f7561134ec2",
+    "url": "/finplan/css/chunk-1a6e9b52.d1fb571b.css"
+  },
+  {
+    "revision": "d67f0a7abb05c600fc49",
+    "url": "/finplan/css/chunk-4069efac.5e90a3ab.css"
+  },
+  {
+    "revision": "d0904f6342258b5d08ee",
+    "url": "/finplan/css/chunk-45d387e6.6aff27f6.css"
+  },
+  {
+    "revision": "f350244d0e3ee3d3b9a6",
+    "url": "/finplan/css/chunk-4814e02a.76874681.css"
+  },
+  {
+    "revision": "229e3e9e354086b19b3c",
+    "url": "/finplan/css/chunk-56a4743d.527573be.css"
+  },
+  {
+    "revision": "ce42fb6947e09f42c005",
+    "url": "/finplan/css/chunk-56a70b20.527573be.css"
+  },
+  {
+    "revision": "3b6d75ae2792ddb1e0ad",
+    "url": "/finplan/css/chunk-5ca77d6a.d58645f3.css"
+  },
+  {
+    "revision": "ee70344d41937e506962",
+    "url": "/finplan/css/chunk-67570c2b.4e0c3e61.css"
+  },
+  {
+    "revision": "f6bebd03cdc31e330515",
+    "url": "/finplan/css/chunk-d8f1e2a4.b00dceea.css"
+  },
+  {
+    "revision": "8b3256cca581a93da2af",
+    "url": "/finplan/css/chunk-feedc266.33a3caf5.css"
+  },
+  {
+    "revision": "0ba4915e75840ed61ffa",
+    "url": "/finplan/css/chunk-vendors.104ddbbe.css"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/finplan/fonts/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/finplan/fonts/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/finplan/fonts/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/finplan/fonts/MaterialIcons-Regular.e79bfd88.eot"
+  },
+  {
+    "revision": "be585b7964de5e00d25e191d254b00f0",
+    "url": "/finplan/img/splashes/apple_splash_750.png"
+  },
+  {
+    "revision": "22410124594cbec2d9630b32054affc3",
+    "url": "/finplan/index.html"
+  },
+  {
+    "revision": "4c9cf2ec7f241780cce9",
+    "url": "/finplan/js/app.f7aa7b2e.js"
+  },
+  {
+    "revision": "35e0cf2fe043a164cc54",
+    "url": "/finplan/js/chunk-051ef01c.85ed14b8.js"
+  },
+  {
+    "revision": "c2486de4b7816df65330",
+    "url": "/finplan/js/chunk-0f8afdc6.23339383.js"
+  },
+  {
+    "revision": "a6e24cd78f7561134ec2",
+    "url": "/finplan/js/chunk-1a6e9b52.53d07eb4.js"
+  },
+  {
+    "revision": "d67f0a7abb05c600fc49",
+    "url": "/finplan/js/chunk-4069efac.9e2e4201.js"
+  },
+  {
+    "revision": "d0904f6342258b5d08ee",
+    "url": "/finplan/js/chunk-45d387e6.d025ab30.js"
+  },
+  {
+    "revision": "f350244d0e3ee3d3b9a6",
+    "url": "/finplan/js/chunk-4814e02a.fdaa4411.js"
+  },
+  {
+    "revision": "229e3e9e354086b19b3c",
+    "url": "/finplan/js/chunk-56a4743d.0ca23b09.js"
+  },
+  {
+    "revision": "ce42fb6947e09f42c005",
+    "url": "/finplan/js/chunk-56a70b20.76dcd616.js"
+  },
+  {
+    "revision": "3b6d75ae2792ddb1e0ad",
+    "url": "/finplan/js/chunk-5ca77d6a.01ce5ff2.js"
+  },
+  {
+    "revision": "ee70344d41937e506962",
+    "url": "/finplan/js/chunk-67570c2b.5dea4485.js"
+  },
+  {
+    "revision": "f6bebd03cdc31e330515",
+    "url": "/finplan/js/chunk-d8f1e2a4.3ea4ee2b.js"
+  },
+  {
+    "revision": "8b3256cca581a93da2af",
+    "url": "/finplan/js/chunk-feedc266.5ead6fec.js"
+  },
+  {
+    "revision": "0ba4915e75840ed61ffa",
+    "url": "/finplan/js/chunk-vendors.384ed95f.js"
+  },
+  {
+    "revision": "e3b5a23e2b3c77ac9e7f97eff12f9385",
+    "url": "/finplan/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/finplan/robots.txt"
+  },
+  {
+    "revision": "17c635356e1bc9013414d393bf0558a0",
+    "url": "/finplan/site.webmanifest"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/finplan/precache-manifest.ff5d05f8e90b8ed41536065af5c69df9.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/finplan/precache-manifest.bb1e14519c9cbbb9aae8e01ea46e54db.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
