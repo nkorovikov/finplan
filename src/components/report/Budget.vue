cf97d995832c8ad1d5ccdb5e3a4d3837b66ba6c3
nkorovikov <korovikov10@gmail.com>
2020-07-24 02:22:21 +0300

    tooltip for ios safari fix

--- src/components/report/Budget.vue
+++ src/components/report/Budget.vue
@@ -1,57 +1,48 @@
 <template>
   <div>
     <template v-if="profile.dailyBudget > 0">
-      <h3>{{ $t('profile.daily-budget') }}</h3>
-      <v-tooltip color="black" top>
+      <v-tooltip color="black" open-on-click :open-on-hover="false" right>
         <template v-slot:activator="{ on, attrs }">
-          <v-progress-linear
-            rounded
-            stream
-            v-bind="attrs"
-            v-on="on"
-            :height="lineWidth"
-            :value="dailyBudgetProgress"
-            :color="dailyBudgetProgress === 100 ? 'red' : 'success'"
-          />
+          <h4 v-bind="attrs" v-on="on">{{ $t('profile.daily-budget') }}</h4>
         </template>
         <span>{{ dailyBudget + '/' + this.profile.dailyBudget }}</span>
       </v-tooltip>
+      <v-progress-linear
+        rounded
+        :height="lineWidth"
+        :value="dailyBudgetProgress"
+        :color="dailyBudgetProgress === 100 ? 'red' : 'success'"
+      />
       <br />
     </template>
     <template v-if="profile.weeklyBudget > 0">
-      <h3>{{ $t('profile.week-budget') }}</h3>
-      <v-tooltip color="black" top>
+      <v-tooltip color="black" open-on-click :open-on-hover="false" right>
         <template v-slot:activator="{ on, attrs }">
-          <v-progress-linear
-            rounded
-            stream
-            v-bind="attrs"
-            v-on="on"
-            :height="lineWidth"
-            :value="weeklyBudgetProgress"
-            :color="weeklyBudgetProgress === 100 ? 'red' : 'success'"
-          />
+          <h4 v-bind="attrs" v-on="on">{{ $t('profile.week-budget') }}</h4>
         </template>
         <span>{{ weeklyBudget + '/' + this.profile.weeklyBudget }}</span>
       </v-tooltip>
+      <v-progress-linear
+        rounded
+        :height="lineWidth"
+        :value="weeklyBudgetProgress"
+        :color="weeklyBudgetProgress === 100 ? 'red' : 'success'"
+      />
       <br />
     </template>
     <template v-if="profile.monthlyBudget > 0">
-      <h3>{{ $t('profile.month-budget') }}</h3>
-      <v-tooltip color="black" top>
+      <v-tooltip color="black" open-on-click :open-on-hover="false" right>
         <template v-slot:activator="{ on, attrs }">
-          <v-progress-linear
-            rounded
-            stream
-            v-bind="attrs"
-            v-on="on"
-            :height="lineWidth"
-            :value="monthlyBudgetProgress"
-            :color="monthlyBudgetProgress === 100 ? 'red' : 'success'"
-          />
+          <h4 v-bind="attrs" v-on="on">{{ $t('profile.month-budget') }}</h4>
         </template>
         <span>{{ monthlyBudget + '/' + this.profile.monthlyBudget }}</span>
       </v-tooltip>
+      <v-progress-linear
+        rounded
+        :height="lineWidth"
+        :value="monthlyBudgetProgress"
+        :color="monthlyBudgetProgress === 100 ? 'red' : 'success'"
+      />
       <br />
     </template>
   </div>
@@ -69,7 +60,7 @@ const profile = namespace("Profile");
 })
 export default class Budget extends Vue {
   private profile: Profile = new Profile("", 0, 0, 0, "ru");
-  private lineWidth = 5;
+  private lineWidth = 14;
 
   @Prop() readonly dailyBudget!: number;
   @Prop() readonly weeklyBudget!: number;
