9d3f169dd611b57d93e0318318e5cf8b24e18699
nkorovikov <korovikov10@gmail.com>
2020-08-13 08:15:18 +0300

    improve report service

--- src/services/ReportService.ts
+++ src/services/ReportService.ts
@@ -9,8 +9,7 @@ export default class ReportService {
         const result: Array<number> = [];
 
         [...Array(7).keys()].forEach(() => {
-            let sum = 0;
-            expenses
+            const sum = expenses
                 .filter(a => {
                     const isOutcome = categories.find(
                         c => a.getCategoryId() === c.getId()
@@ -20,9 +19,9 @@ export default class ReportService {
 
                     return isOutcome && isOutcome.getType() && isUnderInterval;
                 })
-                .forEach(a => {
-                    sum += a.getSum();
-                });
+                .map((e) => e.getSum())
+                .reduce((sum, s) => sum + s, 0);
+
             result.push(sum);
             startOfNow = new Date(startOfNow).setDate(
                 new Date(startOfNow).getDate() - 1
@@ -40,7 +39,7 @@ export default class ReportService {
         let result = 0;
 
         [...Array(daysCount).keys()].forEach(() => {
-            expenses
+            result += expenses
                 .filter(a => {
                     const isOutcome = categories.find(
                         c => a.getCategoryId() === c.getId()
@@ -50,9 +49,8 @@ export default class ReportService {
 
                     return isOutcome && isOutcome.getType() && isUnderInterval;
                 })
-                .forEach(a => {
-                    result += a.getSum();
-                });
+                .map((e) => e.getSum())
+                .reduce((sum, s) => sum + s, 0);
             startOfNow = new Date(startOfNow).setDate(
                 new Date(startOfNow).getDate() - 1
             );
