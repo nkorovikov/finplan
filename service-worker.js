785c43c3ce1c103295afb3f17a304494ca7728f9
nkorovikov <nkorovikov@users.noreply.github.com>
2020-08-13 04:46:40 +0000

    deploy: 8ef091c2419910106b9acb54bbfb280b021041de

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/finplan/precache-manifest.5f592f0bb7e1621b58fa1fe4ca1a86be.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/finplan/precache-manifest.512b6f56c88ce038f49f4de746994caa.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
