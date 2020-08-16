49e46d3431efaaa6be79849e803527073f7b8763
nkorovikov <nkorovikov@users.noreply.github.com>
2020-08-16 22:09:42 +0000

    deploy: 893db5ae1b28d01e40b2cca5db2f541368f009ca

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 importScripts("/finplan/precache-manifest.c5145e2f69c1a1734f212d332ee2de81.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
+// workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 // install new service worker when ok, then reload page.
