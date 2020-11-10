79de43d064cd75a3e278aa3d6f589f2215d79dbe
nkorovikov <korovikov10@gmail.com>
2020-11-10 22:26:36 +0300

    Fix date init

--- src/library/date/DateInitializer.ts
+++ src/library/date/DateInitializer.ts
@@ -2,9 +2,9 @@ export default class DateInitialized {
   initDate(): string {
     return (
       new Date().getFullYear() +
-      (new Date().getMonth() + 1 > 10 ? "-" : "-0") +
+      (new Date().getMonth() + 1 > 9 ? "-" : "-0") +
       (new Date().getMonth() + 1) +
-      (new Date().getDate() > 10 ? "-" : "-0") +
+      (new Date().getDate() > 9 ? "-" : "-0") +
       new Date().getDate()
     );
   }
