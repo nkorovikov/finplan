994e8b6c0ad0f7df9eea0e689bf671222e1a82a5
nkorovikov <korovikov10@gmail.com>
2020-11-13 01:17:10 +0300

    Sentry integrarion

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ import store from "./store";
 import vuetify from "./plugins/vuetify";
 import i18n from "./i18n";
 import axios, { AxiosStatic } from "axios";
+import * as Sentry from "@sentry/browser";
+import { Vue as VueIntegration } from "@sentry/integrations";
+import { Integrations } from "@sentry/tracing";
 
 Vue.config.productionTip = false;
 
@@ -14,6 +17,21 @@ declare module "vue/types/vue" {
   interface Vue {
     $axios: AxiosStatic;
   }
+}
+
+if (process.env.NODE_ENV === 'production') {
+  Sentry.init({
+    dsn: process.env.VUE_APP_SENTRY_DSN,
+    integrations: [
+      new VueIntegration({
+        Vue,
+        tracing: true,
+      }),
+      new Integrations.BrowserTracing(),
+    ],
+  
+    tracesSampleRate: 1.0,
+  });
 }
 
 export const eventBus = new Vue();
