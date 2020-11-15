e6c981feafd323c0a2f174ee043d11d562422aea
nkorovikov <korovikov10@gmail.com>
2020-11-15 19:11:37 +0300

    Firebase and stage

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ import axios, { AxiosStatic } from "axios";
 import * as Sentry from "@sentry/browser";
 import { Vue as VueIntegration } from "@sentry/integrations";
 import { Integrations } from "@sentry/tracing";
+import firebase from "firebase/app";
 
 Vue.config.productionTip = false;
 
@@ -19,7 +20,7 @@ declare module "vue/types/vue" {
   }
 }
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   Sentry.init({
     dsn: process.env.VUE_APP_SENTRY_DSN,
     integrations: [
@@ -29,9 +30,23 @@ if (process.env.NODE_ENV === 'production') {
       }),
       new Integrations.BrowserTracing(),
     ],
-  
+
     tracesSampleRate: 1.0,
   });
+
+  firebase.initializeApp({
+    apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+    authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.VUE_APP_FIREBASE_APP_ID,
+    measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID,
+  });
+  if (!process.env.VUE_APP_STAGE) {
+    firebase.analytics();
+  }
 }
 
 export const eventBus = new Vue();
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "publicPath": "/finplan/",
+  "publicPath": process.env.VUE_APP_STAGE ? "" : "/finplan/",
   "pwa": {
     "appleMobileWebAppCapable": "yes",
     "appleMobileWebAppStatusBarStyle": "black",
